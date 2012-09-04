ace73296703ead30083053beadd21a7d61bb6733
Etienne ANDRE <eti.andre@gmail.com>
2012-09-04 13:42:54 +0200

    ajout zoom, deplacement carte

--- nemeriatools/static/js/carte.js
+++ nemeriatools/static/js/carte.js
@@ -1,5 +1,28 @@
 $(function() {
-    $('select[name="monde"]').change(function() {
-        $(this).parents('form').submit();
+    $('form').children().change(function() {
+        if ($('input[name=drawall]').prop("checked")==true) { $('input[name=alliances]').hide() }
+        else { $('input[name=alliances]').show() }
+        var src="";
+        src+="monde="+document.carteform.monde.value;
+        if (document.carteform.drawall.checked==true) {
+            src+="&drawall=1";
+        }
+        if (document.carteform.alliances.value != "") {
+            for (a in document.carteform.alliances.value.split(/,\s*/)) {
+                src+="&alliance="+document.carteform.alliances.value.split(/,\s*/)[a];
+            }
+        }
+        src+="&h="+(Math.floor($(document).width()-$(document).width()/12*3));
+        xpos=$('input[name=xpos]').prop('value');
+        ypos=$('input[name=ypos]').prop('value');
+        if (isNaN(xpos)) { xpos="100" }
+        if (isNaN(ypos)) { ypos="100" }
+        zoom=parseInt($('select[name=zoom]').prop('value'));
+        src+="&x="+xpos;
+        src+="&y="+ypos;
+        src+="&z="+zoom;
+        $('#carte').prop('src',"/carte/image?"+src);
+        $('#permalink').prop('href',"/carte/image?"+src);
     });
 });
+
--- nemeriatools/views.py
+++ nemeriatools/views.py
@@ -57,32 +57,45 @@ def carte_image(request):
     import Image, ImageDraw
     from math import ceil,floor
     from random import randrange
+    
     monde = request.GET.get('monde')
+    CARTE_X=Monde.objects.get(nom=monde).carte_x
+    CARTE_Y=Monde.objects.get(nom=monde).carte_y
+    colors=[(255,0,0),(0,255,0),(0,0,255),(255,200,0),(255,0,255),(0,255,255),(0,0,0)]
+
+
     todraw=request.GET.getlist('alliance')
     todraw=[x.lower() for x in todraw]
     drawall=request.GET.get('drawall')
     height=int(request.GET.get('h', 1000))
+    zoom=int(request.GET.get('z', 1))
+    ypos=int(request.GET.get('y', 1))
+    xpos=int(request.GET.get('x', 1))
+    
+    print xpos,ypos,zoom
     if height > 3000: height=3000
-    CARTE_X=Monde.objects.get(nom=monde).carte_x
-    CARTE_Y=Monde.objects.get(nom=monde).carte_y
-    colors=[(255,0,0),(0,255,0),(0,0,255),(255,200,0),(255,0,255),(0,255,255),(0,0,0)]
     blocksize=int(height/CARTE_X)
-    height=blocksize*CARTE_X
-    print blocksize
+    height=blocksize*CARTE_X 
+
     im=Image.new('RGB',(height+200,height))
     draw=ImageDraw.Draw(im)
     draw.rectangle([0,0,height+200,height],fill=(250,250,250))
     draw.rectangle([0,0,height,height],fill=(255,255,255))
     
+    blocksize=blocksize*(zoom+1)
+    xpos=xpos-CARTE_X+CARTE_X*zoom
+    ypos=ypos-CARTE_Y+CARTE_Y*zoom
+    print xpos,ypos,zoom
     i=0 # compteur de l'alliance, utilise pour les couleurs et le texte
     for alliance in Alliance.objects.filter(monde__nom__iexact=monde).order_by("classement"):
         if not drawall and not alliance.nom.lower() in todraw: continue # si l'alliance n'est pas demandee, ne pas la dessiner
         try: colors[c] # si y'a pas assez de couleurs, on en rajoute une
         except: colors.append((randrange(0,230),randrange(0,230),randrange(0,230)))
         for ville in Ville.objects.filter(joueur__alliance__nom=alliance.nom,joueur__monde__nom=monde):
-            x=(ville.id%CARTE_Y)*blocksize
-            y=int(CARTE_Y-floor(ville.id/CARTE_Y))*blocksize
-            draw.rectangle([x+1,y+1,x+blocksize-1,y+blocksize-1],fill=colors[i]) # dessin du carre  
+            x=(ville.id%CARTE_Y)*blocksize-xpos
+            y=int(CARTE_Y-floor(ville.id/CARTE_Y))*blocksize-ypos
+
+            draw.rectangle([x+1,y+1,x+blocksize-1,y+blocksize-1],fill=colors[i]) 
         draw.rectangle([height+10,i*30+5,height+35,i*30+25],fill=colors[i]) 
         draw.text((height+50,i*30+10), alliance.nom, fill=colors[i])
         i+=1
