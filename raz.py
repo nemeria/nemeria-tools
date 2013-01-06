553392a6ced4fb84981ea7f94e49c619a5908bc1
Etienne André <eti.andre@gmail.com>
2013-01-06 19:28:35 +0100

    Vrai utilitaire

--- raz.py
+++ raz.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
 from django.core.management import setup_environ
 from nemeria import settings
 setup_environ(settings)
 from nemeriatools.models import *
 
-Ville.objects.all().delete()
-Joueur.objects.all().delete()
-Alliance.objects.all().delete()
+if len(sys.argv) != 2:
+    print "Usage: " + sys.argv[0] + " <monde>"
+    exit(1)
+try:
+    Monde.objects.get(nom=sys.argv[1])
+except:
+    print "Ce monde n'existe pas."
+    exit(1)
+else:
+    print "Suppression des villes..."
+    Ville.objects.filter(monde__nom=sys.argv[1]).delete()
+    print "Suppression des joueurs..."
+    Joueur.objects.filter(monde__nom=sys.argv[1]).delete()
+    print "Suppression des alliances..."
+    Alliance.objects.filter(monde__nom=sys.argv[1]).delete()
+    print "Fait."
+
